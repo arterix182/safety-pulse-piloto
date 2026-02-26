6dbaf8b41857fa8845a5fc1f2c2c95809eaa5cbc
arterix182 <a.ampudia.p@gmail.com>
2026-02-26 15:33:01 -0600

    fallo de codigo

--- netlify/functions/up.js
+++
@@ -1,5 +0,0 @@
-// Compatibility alias: some frontends call /.netlify/functions/up?gmin=...
-module.exports.handler = async (event, context) => {
-  const fn = require('./gmin-lookup');
-  return fn.handler(event, context);
-};
