a54b3b086dc81cd6f7de35e7533a47e2cffb8064
arterix182 <a.ampudia.p@gmail.com>
2026-02-26 14:21:24 -0600

    Delete netlify/functions directory

--- netlify/functions/health.js
+++
@@ -1,5 +0,0 @@
-const { json } = require("./_shared.js");
-exports.handler = async (event) => {
-  if (event.httpMethod === "OPTIONS") return { statusCode: 204, headers: {"access-control-allow-origin":"*","access-control-allow-methods":"GET,POST,OPTIONS","access-control-allow-headers":"content-type"}, body: "" };
-  return json(200, { ok:true, service:"securito-cloud", version:"v40" });
-}
